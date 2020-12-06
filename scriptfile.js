1082faa72471abd9e985d46c7089027162cf65b6
Nitin Saxena <72120832+nitins19@users.noreply.github.com>
2020-12-07 01:34:08 +0530

    Updated2 script file

--- scriptfile.js
+++ scriptfile.js
@@ -3,24 +3,35 @@ const submitButton = document.getElementById("submit");
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("vscode");
-    localStorage.getItem();
+    for (var i = 0; i < localStorage.length; i++) {
+
+        const ul = document.getElementById("listOfPeople1");
+        var li = document.createElement("li");
+        var key = localStorage.key(i);
+        var t = document.createTextNode(JSON.parse(localStorage.getItem(key)).name + " " + JSON.parse(localStorage.getItem(key)).emailId + " " + JSON.parse(localStorage.getItem(key)).phone);
+        li.appendChild(t);
+        ul.appendChild(li);
+
+
+    }
+
 });
 
-if (document.readyState !== "loading") {
+if (document.readyState != "loading") {
     console.log("vscode");
 
-    var keys = Object.keys(localStorage), 
-        i = keys.length; 
+    var keys = Object.keys(localStorage),
+        i = keys.length;
     console.log("keys", keys);
     let stringifiedDetailsOfPeople, detailsOfPeople;
 
-  
+
     while (i--) {
-        
+
         if (keys[i].match(/userDetails/g)) {
-            
+
             stringifiedDetailsOfPeople = localStorage.getItem(keys[i]);
-            console.log("stringifiedDetailsOfPeople",stringifiedDetailsOfPeople);
+            console.log("stringifiedDetailsOfPeople", stringifiedDetailsOfPeople);
             detailsOfPeople = JSON.parse(stringifiedDetailsOfPeople);
             console.log("details", detailsOfPeople);
 
@@ -34,21 +45,24 @@ if (document.readyState !== "loading") {
 submitButton.addEventListener("click", () => {
     const emailId = document.getElementById("email").value;
     const name = document.getElementById("name").value;
+    const phone = document.getElementById("phone").value;
     if (emailId.length > 0 && name.length > 0) {
         const object = {
             name: name,
-            emailId: emailId 
+            emailId: emailId,
+            phone: phone
         };
         localStorage.setItem("userDetails " + emailId, JSON.stringify(object));
-        
+
         addNewLineElement(object);
+
     }
 });
 
 function addNewLineElement(object) {
     const ul = document.getElementById("listOfPeople");
     const li = document.createElement("li");
-    li.appendChild(document.createTextNode(object.name + " " + object.emailId));
+    li.appendChild(document.createTextNode(object.name + " " + object.emailId + " " + object.phone));
     ul.appendChild(li);
     console.log(object);
 }
